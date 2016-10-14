ae7f8c160df09846fec3a0c0b94f55f8f95b991d
agui1940 <agui1940@126.com>
2016-10-14 10:39:38 +0800

    modify by weijiang

--- mssql index rebuild/Excute plan.sql
+++ mssql index rebuild/Excute plan.sql
@@ -1,23 +1,23 @@
-﻿/************************************************************
-Step2,整理索引第二步!在目标服务器上执行
-Author by WeiJiang
-Create Date:2016-9-20
-
-执行前检查事项:
---1，PlanID存在,且内容正确；
-SELECT * FROM [subak01].[KFDB_Management].[dbo].[dbMaintenance] WHERE result=0 AND serverName=@@SERVERNAME ORDER BY startDTime DESC
-
---2，CheckPoint1 处,流水号正确,排序正确,过滤条件和结果正确,如有问题请修改CheckPoint1 处；
-SELECT a.planNo,a.serverName,[db_name],[object_name],index_name,b.index_type_desc,b.partition_number,page_count,CAST(b.page_count AS MONEY)*8/1024/1024 AS 'index_size_GB',
-	fragment_count,CAST(b.fragment_count AS MONEY)*8/1024/1024 AS 'fragment_index_size_GB',avg_fragmentation_in_percent,type,script,b.startDTime,b.endDTime,b.elapsedTime
-	FROM [SUBAK01].[KFDB_Management].[dbo].[dbMaintenance] a
-	LEFT JOIN [SUBAK01].[KFDB_Management].[dbo].[dbMaintenanceDetail] b ON a.planID=b.planID
-	WHERE [elapsedTime] = 0 AND a.result=0 AND a.serverName=@@SERVERNAME
-	ORDER BY b.[type],b.fragment_count DESC,b.page_count DESC;
-
---3，CheckPoint2 ！！！此处默认注释，正式执行前请取消--EXEC (@script)注释；
-    --EXEC (@script); 
-************************************************************/
+﻿/*#############################################
+# Step2,整理索引第二步!在目标服务器上执行
+# Author by WeiJiang
+# Create Date:2016-9-20
+# 
+# 执行前检查事项:
+# 1，PlanID存在,且内容正确；
+# SELECT * FROM [subak01].[KFDB_Management].[dbo].[dbMaintenance] WHERE result=0 AND serverName=@@SERVERNAME ORDER BY startDTime DESC
+# 
+# 2，CheckPoint1 处,流水号正确,排序正确,过滤条件和结果正确,如有问题请修改CheckPoint1 处；
+# SELECT a.planNo,a.serverName,[db_name],[object_name],index_name,b.index_type_desc,b.partition_number,page_count,CAST(b.page_count AS MONEY)*8/1024/1024 AS 'index_size_GB',
+# 	fragment_count,CAST(b.fragment_count AS MONEY)*8/1024/1024 AS 'fragment_index_size_GB',avg_fragmentation_in_percent,type,script,b.startDTime,b.endDTime,b.elapsedTime
+# 	FROM [SUBAK01].[KFDB_Management].[dbo].[dbMaintenance] a
+# 	LEFT JOIN [SUBAK01].[KFDB_Management].[dbo].[dbMaintenanceDetail] b ON a.planID=b.planID
+# 	WHERE [elapsedTime] = 0 AND a.result=0 AND a.serverName=@@SERVERNAME
+# 	ORDER BY b.[type],b.fragment_count DESC,b.page_count DESC;
+# 
+# 3，CheckPoint2 ！！！此处默认注释，正式执行前请取消--EXEC (@script)注释；
+#    --EXEC (@script); 
+##############################################*/
 
 USE master;
 GO
